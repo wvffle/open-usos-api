7e4b49e38fc571817c29958a88f481aed7662488
Kasper Seweryn <github@wvffle.net>
2021-06-22 01:39:05 +0200

    Add session.get_grades()

--- session.py
+++ session.py
@@ -3,10 +3,14 @@ from typing import Optional
 from urllib.parse import urljoin, urlparse, urlencode
 
 import aiohttp as aiohttp
+import re
 
 
 class Session:
     sess: aiohttp.ClientSession
+    name: str
+    index: int
+    csrf_token: str
 
     def __init__(self, usosweb_url, sess: Optional[aiohttp.ClientSession] = None):
         self.sess = aiohttp.ClientSession() if sess is None else sess
@@ -42,4 +46,59 @@ class Session:
 
         async with self.sess.post(post_url, data=data) as resp:
             soup = BeautifulSoup(await resp.text(), features="html.parser")
-            print(soup.prettify())
+
+            # TODO: Check if we are logged in
+
+            self.name = soup.select_one('td:nth-child(2) > b.casmenu').text
+            for script in soup.find_all('script'):
+                if len(script.contents) > 0 and 'JSGLOBALS' in script.contents[0]:
+                    content = script.contents[0]
+                    self.index = int(re.search(r'user_id: "(\d+)"', content).group(1))
+                    self.csrf_token = re.search(r'csrftoken = "(.+?)";', content).group(1)
+                    break
+
+    async def get_grades(self):
+        url = self.controller_url + '?' + urlencode({
+            '_action': 'dla_stud/studia/oceny/index',
+        })
+
+        async with self.sess.get(url) as resp:
+            soup = BeautifulSoup(await resp.text(), features="html.parser")
+
+            tables = soup.select('table.grey > tbody')
+
+            semesters = []
+            grades = []
+
+            for i, tbody in enumerate(tables):
+                if i % 2 == 0:
+                    semesters.append(re.sub(r' - ukryj$', '', tbody.text.strip()))
+                    continue
+
+                subjects = {}
+
+                for tr in tbody.select('tr'):
+                    g = {}
+
+                    for div in tr.select('td:nth-child(3) > div'):
+                        span = div.select_one('span:last-child')
+                        a = div.select_one('a')
+
+                        # NOTE: We're not assigned to any type
+                        if a is None:
+                            continue
+
+                        if 'brak ocen' in span.text:
+                            g[a.text.strip()] = None
+                        elif span.text == 'ZAL':
+                            g[a.text.strip()] = 'ZAL'
+                        else:
+                            g[a.text.strip()] = float(span.text.replace(',', '.'))
+
+                    subjects[tr.select_one('td > a').text.strip()] = g
+
+                grades.append(subjects)
+
+            return dict(zip(semesters, grades))
+
+
